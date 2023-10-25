5e4bdc14344c04b6fdfb732bbfb9620c4446ebb3
Álvaro Fernández Sánchez <alvaro_0211@hotmail.com>
2023-10-25 19:12:01 +0200

    game over

--- src/main.js
+++ src/main.js
@@ -6,53 +6,81 @@ const NO_ES_UN_NUMERO = 0;
 const EL_NUMERO_ES_MAYOR = 1;
 const EL_NUMERO_ES_MENOR = 2;
 const ES_EL_NUMERO_SECRETO = 3;
+const GAME_OVER_MAXIMO_INTENTOS = 4;
+
+const maximoIntentos = 5;
+let numeroIntentos = 0;
+
+const hasSuperadoElNumeroDeIntentos = () => numeroIntentos > maximoIntentos;
+
+const muestraNumeroIntentos = () => {
+  document.getElementById(
+    "intentos"
+  ).innerHTML = `${numeroIntentos} de ${maximoIntentos}`;
+};
+
+document.addEventListener(`DOMContentLoaded`, muestraNumeroIntentos);
+
+const gestionarGameOver = (estado) => {
+  if (estado === GAME_OVER_MAXIMO_INTENTOS) {
+    document.getElementById("comprobar").disabled = true;
+  }
+};
 
 const muestraMensajeComprobacion = (texto, estado) => {
-    let mensaje = '';
+  let mensaje = "";
 
-    switch (estado) {
-        case NO_ES_UN_NUMERO:
-            mensaje = `${texto} no es un número`;
-            break;
-        case EL_NUMERO_ES_MAYOR:
-            mensaje = `El número ${texto} es mayor que el número a acertar`;
-            break;
-        case EL_NUMERO_ES_MENOR:
-            mensaje = `El número ${texto} es menor que el número a acertar`;
-            break;
-        case ES_EL_NUMERO_SECRETO:
-            mensaje = '¡¡¡Enhorabuena, has acertado el número!!!';
-            break;
-        default:
-            mensaje = 'No deberías estar aquí';
-            break;
-    };
+  switch (estado) {
+    case NO_ES_UN_NUMERO:
+      mensaje = `${texto} no es un número`;
+      break;
+    case EL_NUMERO_ES_MAYOR:
+      mensaje = `El número ${texto} es mayor que el número a acertar`;
+      break;
+    case EL_NUMERO_ES_MENOR:
+      mensaje = `El número ${texto} es menor que el número a acertar`;
+      break;
+    case ES_EL_NUMERO_SECRETO:
+      mensaje = "¡¡¡Enhorabuena, has acertado el número!!!";
+      break;
+    case GAME_OVER_MAXIMO_INTENTOS:
+      mensaje = "GAME OVER, has superado el número de intentos";
+      break;
+    default:
+      mensaje = "No deberías estar aquí";
+      break;
+  }
 
-    document.getElementById('resultado').innerHTML = mensaje;
+  document.getElementById("resultado").innerHTML = mensaje;
 };
 
 const comprobarNumero = (texto) => {
-    const numero = parseInt(texto);
-    const esUnNumero = !isNaN(numero);
+  const numero = parseInt(texto);
+  const esUnNumero = !isNaN(numero);
 
-    if (!esUnNumero) {
-        return NO_ES_UN_NUMERO;
-    }
+  if (!esUnNumero) {
+    return NO_ES_UN_NUMERO;
+  }
 
-    if (numero === numeroParaAcertar) {
-        return ES_EL_NUMERO_SECRETO;
-    }
+  if (numero === numeroParaAcertar) {
+    return ES_EL_NUMERO_SECRETO;
+  }
 
-    return numero > numeroParaAcertar ? EL_NUMERO_ES_MAYOR : EL_NUMERO_ES_MENOR;
+  if (hasSuperadoElNumeroDeIntentos()) {
+    return GAME_OVER_MAXIMO_INTENTOS;
+  }
 
+  return numero > numeroParaAcertar ? EL_NUMERO_ES_MAYOR : EL_NUMERO_ES_MENOR;
 };
 
 const handleCompruebaClick = () => {
-    const texto = document.getElementById('numero').value;
-    const estado = comprobarNumero(texto);
-    muestraMensajeComprobacion(texto, estado);
-}
+  const texto = document.getElementById("numero").value;
+  const estado = comprobarNumero(texto);
+  muestraMensajeComprobacion(texto, estado);
+  numeroIntentos++;
+  muestraNumeroIntentos();
+  gestionarGameOver(estado);
+};
 
-
-const botonComprobar = document.getElementById('comprobar');
-botonComprobar.addEventListener('click', handleCompruebaClick);
+const botonComprobar = document.getElementById("comprobar");
+botonComprobar.addEventListener("click", handleCompruebaClick);
